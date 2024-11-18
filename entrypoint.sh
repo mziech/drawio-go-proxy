e7e0057498b782990aeae2c1d60d93d6607006f3
Marco Ziech <marco@ziech.net>
2024-11-18 23:09:15 +0100

    Allow to run as normal user

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ set -eu
 mkdir -p /webroot
 if [ ! -f /webroot/index.html ]; then
   echo "Downloading and installing draw.io ..."
-  mkdir -p /unpack
+  mkdir -p /tmp/unpack
   wget "https://github.com/jgraph/drawio/archive/$DRAWIO_BRANCH.zip"
   unzip "$DRAWIO_BRANCH.zip"
   cd "./drawio-$DRAWIO_BRANCH/src/main/webapp"
   mv * /webroot/
   rm -r /webroot/WEB-INF /webroot/META-INF
   cd /webroot
-  rm -r /unpack
+  rm -r /tmp/unpack
 else
   echo "draw.io already installed."
 fi
