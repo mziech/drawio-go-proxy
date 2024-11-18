ece811b57c0dab15b5f040f1077fbb0518b2d1a5
Marco Ziech <marco@ziech.net>
2024-11-18 22:33:49 +0100

    Attempt to fix test job

--- run-test.sh
+++ run-test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml run --rm test
+docker compose -f docker-compose.test.yml build
+docker compose -f docker-compose.test.yml run --rm test
 rc=$?
-docker-compose -f docker-compose.test.yml logs
-docker-compose -f docker-compose.test.yml down
+docker compose -f docker-compose.test.yml logs
+docker compose -f docker-compose.test.yml down
 
 echo "Leaving test with exit code $rc"
 exit $rc
